aad94892894216a25d16ee03e9d4984e5ee8264e
ronny-valtonen <111150406+ronny-valtonen@users.noreply.github.com>
2025-02-11 12:37:22 -0800

    Update crossmark_database_parser.py

--- crossmark_database_parser.py
+++ crossmark_database_parser.py
@@ -71,10 +71,10 @@ def parse_performance(file, sheet):
         # print(rows)
     
     # Get scores
-    real_row_1 = rows[6]
-    real_row_2 = rows[7]
-    real_row_3 = rows[8]
-    real_row_4 = rows[9]
+    real_row_1 = rows[1]
+    real_row_2 = rows[6]
+    real_row_3 = rows[14]
+    real_row_4 = rows[20]
     crossmark_score = real_row_1[4]
     productivity_score = real_row_2[4]
     creativity_score = real_row_3[4]
@@ -145,114 +145,31 @@ def parse_performance(file, sheet):
     gzip_compress = sub_row_21[-2]
     gzip_uncompress = sub_row_22[-2]
 
-    # Write to the csv now.
-    # my_worksheet.write('A1', 'Iteration/File Name')
-    # my_worksheet.write('B1', 'Test Type')
-    # my_worksheet.write('C1', 'Score/Time')
-
-    # my_worksheet.write('A2', my_file)
-    # my_worksheet.write('A1', 'Crossmark Overall Score')
-    # my_worksheet.write('AB1', my_file)
     my_worksheet.write('A2', crossmark_score)
-
-    # my_worksheet.write('A3', my_file)
-    # my_worksheet.write('B3', 'Productivity Score')
     my_worksheet.write('B2', productivity_score)
-
-    # my_worksheet.write('A4', my_file)
-    # my_worksheet.write('B4', 'Creativity Score')
     my_worksheet.write('C2', creativity_score)
-
-    # my_worksheet.write('A5', my_file)
-    # my_worksheet.write('B5', 'Responsiveness Score')
     my_worksheet.write('D2', responsiveness_score)
-
-    # my_worksheet.write('A6', my_file)
-    # my_worksheet.write('B6', 'zstd_uncompress_legacy')
     my_worksheet.write('E2', zstd_uncompress_legacy)
-
-    # my_worksheet.write('A7', my_file)
-    # my_worksheet.write('B7', 'random_read')
     my_worksheet.write('F2', random_read)
-
-    # my_worksheet.write('A8', my_file)
-    # my_worksheet.write('B8', 'black_scholes_serial')
     my_worksheet.write('G2', black_scholes_serial)
-
-    # my_worksheet.write('A9', my_file)
-    # my_worksheet.write('B9', 'string_search')
     my_worksheet.write('H2', string_search)
-
-    # my_worksheet.write('A10', my_file)
-    # my_worksheet.write('B10', 'random_write')
     my_worksheet.write('I2', random_write)
-
-    # my_worksheet.write('A11', my_file)
-    # my_worksheet.write('B11', 'object_detection')
     my_worksheet.write('J2', object_detection)
-
-    # my_worksheet.write('A12', my_file)
-    # my_worksheet.write('B12', 'ef_face_recognition')
     my_worksheet.write('K2', ef_face_recognition)
-
-    # my_worksheet.write('A13', my_file)
-    # my_worksheet.write('B13', 'zstd_compress_legacy')
     my_worksheet.write('L2', zstd_compress_legacy)
-
-    # my_worksheet.write('A14', my_file)
-    # my_worksheet.write('B14', 'zstd_uncompress_streaming')
     my_worksheet.write('M2', zstd_uncompress_streaming)
-
-    # my_worksheet.write('A15', my_file)
-    # my_worksheet.write('B15', 'fdt_by_medianflow_tracker')
     my_worksheet.write('N2', fdt_by_medianflow_tracker)
-
-    # my_worksheet.write('A16', my_file)
-    # my_worksheet.write('B16', 'black_scholes_parallel')
     my_worksheet.write('O2', black_scholes_parallel)
-
-    # my_worksheet.write('A17', my_file)
-    # my_worksheet.write('B17', 'create_sqlite_blob')
     my_worksheet.write('P2', create_sqlite_blob)
-
-    # my_worksheet.write('A18', my_file)
-    # my_worksheet.write('B18', 'video_colorization')
     my_worksheet.write('Q2', video_colorization)
-
-    # my_worksheet.write('A19', my_file)
-    # my_worksheet.write('B19', 'external_sort')
     my_worksheet.write('R2', external_sort)
-
-    # my_worksheet.write('A20', my_file)
-    # my_worksheet.write('B20', 'chacha20_encrypt_openssl')
     my_worksheet.write('S2', chacha20_encrypt_openssl)
-
-    # my_worksheet.write('A21', my_file)
-    # my_worksheet.write('B21', 'colorization')
     my_worksheet.write('T2', colorization)
-
-    # my_worksheet.write('A22', my_file)
-    # my_worksheet.write('B22', 'hdr_stitch')
     my_worksheet.write('U2', hdr_stitch)
-
-    # my_worksheet.write('A23', my_file)
-    # my_worksheet.write('B23', 'aes_gcm_encrypt_mt')
     my_worksheet.write('V2', aes_gcm_encrypt_mt)
-
-    # my_worksheet.write('A24', my_file)
-    # my_worksheet.write('B24', 'memory_workload')
     my_worksheet.write('W2', memory_workload)
-
-    # my_worksheet.write('A25', my_file)
-    # my_worksheet.write('B25', 'chacha20_decrypt_openssl')
     my_worksheet.write('X2', chacha20_decrypt_openssl)
-
-    # my_worksheet.write('A26', my_file)
-    # my_worksheet.write('B26', 'gzip_compress')
     my_worksheet.write('Y2', gzip_compress)
-
-    # my_worksheet.write('A27', my_file)
-    # my_worksheet.write('B27', 'gzip_uncompress')
     my_worksheet.write('Z2', gzip_uncompress)
 
     
@@ -346,37 +263,7 @@ def transposer():
     columns = sheet["AA1"].value = "gzip_compress"
     columns = sheet["AB1"].value = "gzip_uncompress"
     wb.save("combined_data.xlsx")
-    # print("Creating CSV File.")
-    # csv.writer('transposed.csv', dialect='excel')
-
-    # overall_score = [['2345', '3465', '2344', '3454', '3452', '2342', '34521'],
-    #                     ['2345', '3465', '2344', '3454', '3452', '2342', '34521'],
-    #                     ['2345', '3465', '2344', '3454', '3452', '2342', '34521'],
-    #                     ['2345', '3465', '2344', '3454', '3452', '2342', '34521']]
-
-    # file = open('transposed.csv' 'a+', newline = '')
-    # workbook = xlsxwriter.Workbook('transpose.xlsx')
-    # my_worksheet = workbook.add_worksheet()
-    # my_worksheet.write("A1", "Test")
-
-    # with file:
-    #     write = csv.writer(file)
-    #     write.writerows(overall_score)
-
     print("Transposing complete.")
-    
-
-    # Lists out every row of the excel sheet.
-    # wrkbk = openpyxl.load_workbook('combined_data.xlsx')
-    # sh = wrkbk.active
-
-    # for i in range(1, sh.max_row+1):
-    #     print("\n")
-    #     print("Row ", i, " data:")
-
-    #     for j in range(1, sh.max_column+1):
-    #         cell_obj = sh.cell(row = i, column = j)
-    #         print(cell_obj.value, end=" ")
 
 # Program driver.
 def main():
